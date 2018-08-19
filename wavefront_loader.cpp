63cf6d02919058c2a1e6321475779b9361ecd41c
lincolnbphillips001 <37947122+lincolnbphillips001@users.noreply.github.com>
2018-08-19 17:29:30 +1000

    Update wavefront_loader.cpp

--- wavefront_loader.cpp
+++ wavefront_loader.cpp
@@ -1,44 +1,32 @@
-#include <SDL/SDL.h>
-#include <GL/gl.h>
-#include <GL/glu.h>
-#include <cstdlib>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <fstream>
-#include <cstdio>
+#include "functions.h"
 
-struct coordinate{
-  float x, y, z;
-  coordinate(float a, float b, float c) : x(a), y(b), z(c) {};
-};
+coordinate::coordinate(float a, float b, float c) {
+  x = a;
+  y = b;
+  z = c;
+}
 
-struct face{
-  int facenum;
-  bool four;
-  int faces[4];
-  face(int facen, int f1, int f2, int f3) : facenum(facen) {
-    faces[0] = f1;
-    faces[1] = f2;
-    faces[2] = f3;
-    four = false;
-  }
-  face(int facen, int f1, int f2, int f3, int f4) : facenum(facen) {
-    faces[0] = f1;
-    faces[1] = f2;
-    faces[2] = f3;
-    faces[3] = f4;
-    four = true;
-  }
-};
+face::face(int facen, int f1, int f2, int f3) {
+  facenum = facen;
+  faces[0] = f1;
+  faces[1] = f2;
+  faces[2] = f3;
+  four = false;
+}
 
-
-float angle = 0.0;
+face::face(int facen, int f1, int f2, int f3, int f4) {
+  facenum = facen;
+  faces[0] = f1;
+  faces[1] = f2;
+  faces[2] = f3;
+  faces[3] = f4;
+  four = true;
+}
 
 int loadObject(const char* filename)
 {
   std::vector<std::string*> coord;
-  std::vector<coordinate*> vextex;
+  std::vector<coordinate*> vertex;
   std::vector<face*> faces;
   std::vector<coordinate*> normals;
   std::ifstream in(filename);
@@ -50,8 +38,8 @@ int loadObject(const char* filename)
   char buf[256];
   while (!in.eof())
   {
-    in.getline(c, 256);
-    coord.push_back(new std::string(c));
+    in.getline(buf, 256);
+    coord.push_back(new std::string(buf));
   }
   for (int i=0; i < coord.size(); i++)
   {
@@ -71,74 +59,121 @@ int loadObject(const char* filename)
       sscanf(coord[i]->c_str(), "vn %f %f %f", &tmpx, &tmpy, &tmpz);
       normals.push_back(new coordinate(tmpx, tmpy, tmpz));
     }
-  }
-
-
-}
-
-
-void init(void)
-{
-  glClearColor(0.0, 0.0, 0.0, 1.0);
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  gluPerspective(45, 640.0/480.0, 1.0, 500.0);
-  glMatrixMode(GL_MODELVIEW);
-  glEnable(GL_DEPTH_TEST);
-  glEnable(GL_TEXTURE_2D);
-  tex = loadTexture("images/brick.bmp");
-}
-
-void display(void) {
-
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  glLoadIdentity();
-  glTranslatef(0.0, 0.0, -5.0);
-  glRotatef(angle, 1.0, 1.0, 1.0);
-  glBindTexture(GL_TEXTURE_2D, tex);
-
-  glBegin(GL_QUADS);
-    glTexCoord2f(0.0, 2.0);
-    glVertex3f(-2.0, 2.0, 0.0);
-    glTexCoord2f(0.0, 0.0);
-    glVertex3f(-2.0, -2.0, 0.0);
-    glTexCoord2f(2.0, 0.0);
-    glVertex3f(2.0, -2.0, 0.0);
-    glTexCoord2f(2.0, 2.0);
-    glVertex3f(2.0, 2.0, 0.0);
-  glEnd();
-}
-
-int main(int argc, char** argv) {
-
-  SDL_Init(SDL_INIT_EVERYTHING);
-  SDL_Surface* screen;
-  screen = SDL_SetVideoMode(640,480,32,SDL_SWSURFACE|SDL_OPENGL);
-  bool running = true;
-  const int FPS = 30;
-  Uint32 start;
-  init();
-  while(running) {
-    start = SDL_GetTicks();
-    SDL_Event event;
-    while(SDL_PollEvent(&event)) {
-      switch(event.type) {
-        case SDL_QUIT:
-            running = false;
-            break;
+    else if ((*coord[i])[0] == 'f')
+    {
+      int a, b, c, d, e;
+      if (count(coord[i]->begin(), coord[i]->end(), ' ') == 4)
+      {
+        sscanf(coord[i]->c_str(), "f %d//%d %d//%d %d//%d %d//%d", &a, &b, &c, &b, &d, &b, &e, &b);
+        faces.push_back(new face(b, a, c, d, e));
+      }
+      else
+      {
+        sscanf(coord[i]->c_str(), "f %d//%d %d//%d %d//%d", &a, &b, &c, &b, &d, &b);
+        faces.push_back(new face(b, a, c, d));
       }
     }
-    display();
-    SDL_GL_SwapBuffers();
-    angle = angle + 0.5;
-    if(angle > 360) {
-      angle = angle - 360;
-    }
-
-    if((1000/FPS) > (SDL_GetTicks()-start)) {
-      SDL_Delay((1000/FPS)-(SDL_GetTicks()-start));
+  }
+  //draw
+  int num;
+  num = glGenLists(1);
+  glNewList(num, GL_COMPILE);
+  for (int i=0; i < faces.size(); i++)
+  {
+    if (faces[i]->four) {
+      glBegin(GL_QUADS);
+        glNormal3f(
+          normals[faces[i]->facenum-1]->x,
+          normals[faces[i]->facenum-1]->y,
+          normals[faces[i]->facenum-1]->z);
+        glVertex3f(
+          vertex[faces[i]->faces[0]-1]->x,
+          vertex[faces[i]->faces[0]-1]->y,
+          vertex[faces[i]->faces[0]-1]->z);
+        glVertex3f(
+          vertex[faces[i]->faces[1]-1]->x,
+          vertex[faces[i]->faces[1]-1]->y,
+          vertex[faces[i]->faces[1]-1]->z);
+        glVertex3f(
+          vertex[faces[i]->faces[2]-1]->x,
+          vertex[faces[i]->faces[2]-1]->y,
+          vertex[faces[i]->faces[2]-1]->z);
+        glVertex3f(
+          vertex[faces[i]->faces[3]-1]->x,
+          vertex[faces[i]->faces[3]-1]->y,
+          vertex[faces[i]->faces[3]-1]->z);
+      glEnd();
+    } else {
+      glBegin(GL_TRIANGLES);
+        glNormal3f(
+          normals[faces[i]->facenum-1]->x,
+          normals[faces[i]->facenum-1]->y,
+          normals[faces[i]->facenum-1]->z);
+        glVertex3f(
+          vertex[faces[i]->faces[0]-1]->x,
+          vertex[faces[i]->faces[0]-1]->y,
+          vertex[faces[i]->faces[0]-1]->z);
+        glVertex3f(
+          vertex[faces[i]->faces[1]-1]->x,
+          vertex[faces[i]->faces[1]-1]->y,
+          vertex[faces[i]->faces[1]-1]->z);
+        glVertex3f(
+          vertex[faces[i]->faces[2]-1]->x,
+          vertex[faces[i]->faces[2]-1]->y,
+          vertex[faces[i]->faces[2]-1]->z);
+      glEnd();
     }
   }
-  SDL_Quit();
-  return 0;
+  glEndList();
+  for (int i=0; i < coord.size(); i++)
+    delete coord[i];
+  for (int i=0; i < faces.size(); i++)
+    delete faces[i];
+  for (int i=0; i < normals.size(); i++)
+    delete normals[i];
+  for (int i=0; i < vertex.size(); i++)
+    delete vertex[i];
+  return num;
+}
+
+void drawCube(float sizeOfCube)
+{
+  glBegin(GL_QUADS);
+    // front face
+    glColor3f(1.0,0.0,0.0);
+    glVertex3f(sizeOfCube/2,sizeOfCube/2,sizeOfCube/2);
+    glVertex3f(-sizeOfCube/2,sizeOfCube/2,sizeOfCube/2);
+    glVertex3f(-sizeOfCube/2,-sizeOfCube/2,sizeOfCube/2);
+    glVertex3f(sizeOfCube/2,-sizeOfCube/2,sizeOfCube/2);
+    // left face
+    glColor3f(0.0,1.0,0.0);
+    glVertex3f(-sizeOfCube/2,sizeOfCube/2,sizeOfCube/2);
+    glVertex3f(-sizeOfCube/2,-sizeOfCube/2,sizeOfCube/2);
+    glVertex3f(-sizeOfCube/2,-sizeOfCube/2,-sizeOfCube/2);
+    glVertex3f(-sizeOfCube/2,sizeOfCube/2,-sizeOfCube/2);
+    // back face
+    glColor3f(0.0,0.0,1.0);
+    glVertex3f(sizeOfCube/2,sizeOfCube/2,-sizeOfCube/2);
+    glVertex3f(-sizeOfCube/2,sizeOfCube/2,-sizeOfCube/2);
+    glVertex3f(-sizeOfCube/2,-sizeOfCube/2,-sizeOfCube/2);
+    glVertex3f(sizeOfCube/2,-sizeOfCube/2,-sizeOfCube/2);
+    // right face
+    glColor3f(1.0,1.0,0.0);
+    glVertex3f(sizeOfCube/2,sizeOfCube/2,sizeOfCube/2);
+    glVertex3f(sizeOfCube/2,-sizeOfCube/2,sizeOfCube/2);
+    glVertex3f(sizeOfCube/2,-sizeOfCube/2,-sizeOfCube/2);
+    glVertex3f(sizeOfCube/2,sizeOfCube/2,-sizeOfCube/2);
+    // top face
+    glColor3f(1.0,0.0,1.0);
+    glVertex3f(sizeOfCube/2,sizeOfCube/2,sizeOfCube/2);
+    glVertex3f(-sizeOfCube/2,sizeOfCube/2,sizeOfCube/2);
+    glVertex3f(-sizeOfCube/2,sizeOfCube/2,-sizeOfCube/2);
+    glVertex3f(sizeOfCube/2,sizeOfCube/2,-sizeOfCube/2);
+    // bottom face
+    glColor3f(0.0,1.0,1.0);
+    glVertex3f(sizeOfCube/2,-sizeOfCube/2,sizeOfCube/2);
+    glVertex3f(-sizeOfCube/2,-sizeOfCube/2,sizeOfCube/2);
+    glVertex3f(-sizeOfCube/2,-sizeOfCube/2,-sizeOfCube/2);
+    glVertex3f(sizeOfCube/2,-sizeOfCube/2,-sizeOfCube/2);
+  glEnd();
 }
